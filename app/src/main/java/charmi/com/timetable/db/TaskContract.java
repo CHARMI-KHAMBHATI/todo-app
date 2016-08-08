7278e7279ce31ea5c25e7182f1d59d9b38d693d0
Charmi Khambhati <incharmi.ck@gmail.com>
2016-08-08 17:59:28 +0530

    First commit

---
+++ app/src/main/java/charmi/com/timetable/db/TaskContract.java
@@ -0,0 +1,15 @@
+package charmi.com.timetable.db;
+
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "charmi.com.timetable.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
